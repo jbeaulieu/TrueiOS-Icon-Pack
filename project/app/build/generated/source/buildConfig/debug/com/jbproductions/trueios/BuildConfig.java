b6189f97d608ffe338be941fcb5720d94e8bca08
Jon Beaulieu <jbeaulieu@mit.edu>
2016-04-03 13:19:37 -0400

    minor layout bugfix; update app icon

--- project/app/build/generated/source/buildConfig/debug/com/jbproductions/trueios/BuildConfig.java
+++ project/app/build/generated/source/buildConfig/debug/com/jbproductions/trueios/BuildConfig.java
@@ -5,7 +5,7 @@ package com.jbproductions.trueios;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.jbproductions.iosicons";
+  public static final String APPLICATION_ID = "com.jbproductions.trueios";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
